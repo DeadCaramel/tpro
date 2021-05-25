3c1c7b54ebfded3bd913d953066ac8fb367c9318
umdeadcaramel <pal5686sanjana@gmail.com>
2021-05-25 10:47:33 +0800

    second commit

---
+++ src/main/java/com/chong/tpro/TproApplication.java
@@ -0,0 +1,13 @@
+package com.chong.tpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chong/tpro/TproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chong.tpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
